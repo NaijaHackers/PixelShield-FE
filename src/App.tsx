942f63327c83769b81461ea963baa9fcb866d963
Editor FRed <amdevfred@gmail.com>
2025-02-22 20:43:17 +0100

    animated routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import { AnimatePresence, motion } from "framer-motion";
 import { ROUTES } from "@common/routes";
 import ProtectedRoute from "@components/ProtectedRoute";
 
 const AppRoutes = () => {
+	const location = useLocation();
+
 	return (
-		<>
-			<Routes>
+		<AnimatePresence mode="wait">
+			<Routes location={location} key={location.pathname}>
 				{Object.values(ROUTES).map(
 					({
 						PATH,
@@ -17,21 +20,40 @@ const AppRoutes = () => {
 							key={PATH}
 							path={PATH}
 							element={
-								<ProtectedRoute
-									element={ELEMENT}
-									requiresAuth={REQUIRES_AUTH}
-									authPage={IS_AUTH_PAGE}
-								/>
+								<motion.div
+									className="w-full h-full overflow-hidden flex"
+									initial={{ opacity: 0, x: 50 }}
+									animate={{ opacity: 1, x: 0 }}
+									exit={{ opacity: 0, x: -50 }}
+									transition={{ duration: 0.3 }}
+								>
+									<ProtectedRoute
+										element={ELEMENT}
+										requiresAuth={REQUIRES_AUTH}
+										authPage={IS_AUTH_PAGE}
+									/>
+								</motion.div>
 							}
 						/>
 					)
 				)}
-				<Route path="*" element={<div>404</div>} />
+				<Route
+					path="*"
+					element={
+						<motion.div
+							initial={{ opacity: 0 }}
+							animate={{ opacity: 1 }}
+							exit={{ opacity: 0 }}
+							transition={{ duration: 0.3 }}
+						>
+							<div>404</div>
+						</motion.div>
+					}
+				/>
 			</Routes>
-			{/* OTHER STUFFS HERE like toast container */}
-		</>
-	)
-}
+		</AnimatePresence>
+	);
+};
 
 function App() {
 	return (
