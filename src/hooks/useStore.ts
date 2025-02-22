a58a356bdbe6c41223ddb77b02635b8dd1c5f5ac
Editor FRed <amdevfred@gmail.com>
2025-02-22 18:19:10 +0100

    `Added setUploadedFiles action to useStore hook, updated UploadPage to use it, and added UI elements for uploaded files`

--- src/hooks/useStore.ts
+++ src/hooks/useStore.ts
@@ -6,6 +6,7 @@ export interface IStore {
 
 export interface IStoreActions {
     addUploadedFiles: (files: File[]) => void;
+    setUploadedFiles: (files: File[]) => void;
 }
 
 const ALLOWED_IMAGE_EXTENSIONS = ['jpg', 'jpeg', 'png', 'webp'];
@@ -15,7 +16,9 @@ const useStore = create<IStore & IStoreActions>((set) => ({
     uploadedFiles: [],
 
     addUploadedFiles: (files: File[]) =>
-        set((state) => ({ uploadedFiles: [...state.uploadedFiles, ...files] }))
+        set((state) => ({ uploadedFiles: [...state.uploadedFiles, ...files] })),
+    setUploadedFiles: (files: File[]) =>
+        set(() => ({ uploadedFiles: files }))
 }));
 
 export { ALLOWED_IMAGE_EXTENSIONS, ALLOWED_VIDEO_EXTENSIONS };
--- src/resources/pages/Home/Upload/index.tsx
+++ src/resources/pages/Home/Upload/index.tsx
@@ -3,30 +3,40 @@ import { FileRejection, useDropzone } from "react-dropzone";
 import Container from "@components/Container";
 import HomeLayout from "@resources/layouts/Home";
 import useStore, { ALLOWED_IMAGE_EXTENSIONS } from "@/hooks/useStore";
+import AnimatedButton from "@components/AnimatedButton";
+import ChevronLeft from "@components/ChevronLeft";
+import { Link } from "react-router-dom";
+import { ROUTES } from "@common/routes";
 
 const UploadPage = () => {
 
-    const addUploadedFiles = useStore(state => state.addUploadedFiles)
+    const setUploadedFiles = useStore(state => state.setUploadedFiles)
     const uploadedFiles = useStore(state => state.uploadedFiles)
 
     const onDrop = useCallback((acceptedFiles: File[], fileRejections: FileRejection[]) => {
         console.log("Accepted files:", acceptedFiles);
         console.log("Rejected files:", fileRejections);
-        addUploadedFiles(acceptedFiles)
-    }, []);
+        setUploadedFiles(acceptedFiles)
+    }, [setUploadedFiles]);
 
     const { getRootProps, getInputProps, isDragActive } = useDropzone({
         onDrop,
-        accept: { image: ALLOWED_IMAGE_EXTENSIONS }, // Accept only image files
+        accept: { image: ALLOWED_IMAGE_EXTENSIONS }, // Accept only image files,
+        maxFiles: 1
     });
+
+    const createObjectURL = (file: File) => URL.createObjectURL(file)
 
     return (
         <HomeLayout>
-            <Container className=" p-5 flex-grow">
+            <Container className="flex-grow">
                 <div className="grid grid-cols-1 lg:grid-cols-3 gap-4  min-h-full">
-                    <div className="col-span-1 flex flex-col gap-5">
+                    <div className="col-span-1 flex flex-col gap-5 py-4">
                         <div className="flex items-center justify-between">
-                            <span className="">Upload Images</span>
+                            <h3 className="text-[25px] font-bold">Upload Images</h3>
+                            <Link to={ROUTES['LANDING_PAGE']['PATH']}>
+                                <ChevronLeft />
+                            </Link>
                         </div>
 
                         {/* Drag and drop area */}
@@ -51,9 +61,18 @@ const UploadPage = () => {
                             )}
                         </div>
 
-                        <div className="">
-                            HELLO WORLD {uploadedFiles.length}
-                        </div>
+                        {uploadedFiles.length ?
+                            <div className="w-full flex h-[81px] items-center p-4 bg-(--secondary-blue) rounded-xl">
+                                <div className="flex h-full">
+                                    <img src={createObjectURL(uploadedFiles[0])} alt="" />
+                                </div>
+                                <div className="flex flex-grow">
+                                    <AnimatedButton >
+                                        Upload
+                                    </AnimatedButton>
+                                </div>
+                            </div> : null
+                        }
                     </div>
                     <div className="col-span-1 lg:col-span-2 bg-(--secondary-blue)">
                         sd
